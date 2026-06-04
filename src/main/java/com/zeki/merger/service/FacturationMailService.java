eee9ebc651e30b168f0148461ce68ee9f93fe416
Zekimertincc <Zekimertinc@gmail.com>
2026-06-04 15:58:25 +0200

    recupnumfact pour facturation

--- src/main/java/com/zeki/merger/service/FacturationMailService.java
+++ src/main/java/com/zeki/merger/service/FacturationMailService.java
@@ -282,11 +282,15 @@ public class FacturationMailService {
             StringBuilder vbsContent = new StringBuilder();
             vbsContent.append("Set ol = CreateObject(\"Outlook.Application\")\n");
             vbsContent.append("Set mail = ol.CreateItem(0)\n");
+            vbsContent.append("Dim targetAcc\n");
             vbsContent.append("For Each acc In ol.Session.Accounts\n");
             vbsContent.append("  If InStr(LCase(acc.SmtpAddress), \"cabinetphenix\") > 0 Then\n");
-            vbsContent.append("    mail.SendUsingAccount = acc\n");
+            vbsContent.append("    Set targetAcc = acc\n");
             vbsContent.append("  End If\n");
             vbsContent.append("Next\n");
+            vbsContent.append("If Not IsEmpty(targetAcc) Then\n");
+            vbsContent.append("  mail.SendUsingAccount = targetAcc\n");
+            vbsContent.append("End If\n");
             vbsContent.append("mail.To = \"").append(safeTo).append("\"\n");
             vbsContent.append("mail.Subject = \"").append(safeSubject).append("\"\n");
             vbsContent.append("mail.HTMLBody = \"").append(safeHtml).append("\"\n");
